c96f05c67ca958c36f8ba4c9f2e3103a88092ee4
locnnh <locnnh@sample.com>
2022-06-19 10:00:18 +0700

    add file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const browserDetail = navigator.userAgent;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h4>{browserDetail}</h4>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
